f2b5ee63d0abb5471feb46d9a8f3db8c5844271f
Fjanza <franciscojanza001@gmail.com>
2022-02-22 12:14:35 -0300

    implementacion de lo hecho anteriormente como funcion

--- Python/Mapa.py
+++ Python/Mapa.py
@@ -1,30 +1,19 @@
+from pygame import init
 import requests     #para poder conectarnos con la api 
 import urllib       #para poder manejar mejor las urls
 
-api_url = "http://www.mapquestapi.com/directions/v2/route?"
+def Mapa(origin,destination):
+    api_url = "http://www.mapquestapi.com/directions/v2/route?"
 
-key = "DXOzV2WjMEodSemHC1AvVDwjiGMhpNqH"
+    key = "DXOzV2WjMEodSemHC1AvVDwjiGMhpNqH"
 
-while True:
-
-    origin = "Buenos Aires"
-
-    destination = "Mendoza"
-
-    """
     origin = input("ingrese el origen: ")
-    if origin == 'q':
-        break
+
     destination = input("ingrese el destino: ")
-    if destination == 'q':
-        break
-
-    """
-
 
     url = api_url + urllib.parse.urlencode({"key":key,"from":origin,"to":destination,"unit":"K","destinationManeuverDisplay":True,"locale":"es_MX"})
     json_data = requests.get(url).json()
-    
+
     status_code = json_data["info"]["statuscode"]
 
     if status_code == 0:
@@ -32,16 +21,35 @@ while True:
         duracion_viaje = json_data["route"]["formattedTime"]
         largo_viaje = json_data["route"]["distance"]
 
-        indicaciones = []
+        indicaciones_data = []
         
-        indicaciones.append(json_data["route"]["legs"][0]["origNarrative"])
+        indicaciones_data.append(json_data["route"]["legs"][0]["origNarrative"])
 
 
         for each in json_data["route"]["legs"][0]["maneuvers"]:
             
             
-            indicaciones.append((each["distance"],each["narrative"]))  #guardo en cuantos km flatan para tal accion ej en 1km girar a la derecha
+            indicaciones_data.append((each["distance"],each["narrative"]))  #guardo en cuantos km flatan para tal accion ej en 1km girar a la derecha
 
-        print(indicaciones[1][1])
+        
+        indicaciones = ""
 
-    break
+        indicaciones = indicaciones + indicaciones_data[0]
+
+        for i in range(1,len(indicaciones_data)):
+            if indicaciones_data[i][0] < 1:
+                indicaciones = indicaciones+ "En " + str(indicaciones_data[i][0]*100) + " metros " + indicaciones_data[i][1] + " "
+            else:
+                indicaciones = indicaciones+ "En " + str(indicaciones_data[i][0]) + " Km " + indicaciones_data[i][1] + " " 
+
+        info = []
+
+        info.append(origin)
+        info.append(destination)
+        info.append(indicaciones)
+
+        return(info)
+
+    else:
+        print("status code error")
+        return
